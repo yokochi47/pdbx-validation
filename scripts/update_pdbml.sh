83e3681f678fdd25b7df2afa8e98b0d58ce84178
yokochi47 <yokochi47@gmail.com>
2018-02-08 17:44:09 +0900

    Update scripts

--- scripts/update_pdbml.sh
+++ scripts/update_pdbml.sh
@@ -19,23 +19,33 @@ if [ $weekday -ge 1 ] && [ $weekday -le 4 ] ; then
  rsync -avz --delete --dry-run ftp.pdbj.org::ftp_data/structures/divided/$SRC_DIR/ $SRC_DIR | grep xml.gz | cut -d '/' -f 2 | cut -d '-' -f 1 > $rsync_log
 
  if [ -d $XML_DIR ] ; then
-  while read pdb_id ; do ; rm -f $XML_DIR/$pdb_id-noatom.xml ; done < $rsync_log
+  while read pdb_id ; do
+   rm -f $XML_DIR/$pdb_id-noatom.xml
+  done < $rsync_log
  fi
 
  if [ -d $PDBML_EXT ] ; then
-  while read pdb_id ; do ; rm -f $PDBML_EXT/$pdb_id-noatom-ext.xml ; done < $rsync_log
+  while read pdb_id ; do
+   rm -f $PDBML_EXT/$pdb_id-noatom-ext.xml
+  done < $rsync_log
  fi
 
  if [ -d $VALID_INFO_ALT ] ; then
-  while read pdb_id ; do ; rm -f $VALID_INFO_ALT/$pdb_id-validation-alt.xml ; done < $rsync_log
+  while read pdb_id ; do
+   rm -f $VALID_INFO_ALT/$pdb_id-validation-alt.xml
+  done < $rsync_log
  fi
 
  if [ -d $PDBML_VALID ] ; then
-  while read pdb_id ; do ; rm -f $PDBML_VALID/$pdb_id-validation-full.xml ; done < $rsync_log
+  while read pdb_id ; do
+  rm -f $PDBML_VALID/$pdb_id-validation-full.xml
+  done < $rsync_log
  fi
 
  if [ -d $RDF_VALIT ] ; then
-  while read pdb_id ; do ; rm -f $RDF_VALID/$pdb_id-validation.rdf ; done < $rsync_log
+  while read pdb_id ; do
+   rm -f $RDF_VALID/$pdb_id-validation.rdf
+  done < $rsync_log
  fi
 
  rm -f $rsync_log
--- scripts/update_validation.sh
+++ scripts/update_validation.sh
@@ -19,23 +19,33 @@ if [ $weekday -ge 1 ] && [ $weekday -le 4 ] ; then
  rsync -rlpt -v -z --delete --dry-run ftp.pdbj.org::ftp/$SRC_DIR . | grep xml.gz | cut -d '/' -f 3 > $rsync_log
 
  if [ -d $XML_DIR ] ; then
-  while read pdb_id ; do ; rm -f $XML_DIR/$pdb_id"_validation.xml" ; done < $rsync_log
+  while read pdb_id ; do
+   rm -f $XML_DIR/$pdb_id"_validation.xml"
+  done < $rsync_log
  fi
 
  if [ -d $PDBML_EXT ] ; then
-  while read pdb_id ; do ; rm -f $PDBML_EXT/$pdb_id-noatom-ext.xml ; done < $rsync_log
+  while read pdb_id ; do
+   rm -f $PDBML_EXT/$pdb_id-noatom-ext.xml
+  done < $rsync_log
  fi
 
  if [ -d $VALID_INFO_ALT ] ; then
-  while read pdb_id ; do ; rm -f $VALID_INFO_ALT/$pdb_id-validation-alt.xml ; done < $rsync_log
+  while read pdb_id ; do
+   rm -f $VALID_INFO_ALT/$pdb_id-validation-alt.xml
+  done < $rsync_log
  fi
 
  if [ -d $PDBML_VALID ] ; then
-  while read pdb_id ; do ; rm -f $PDBML_VALID/$pdb_id-validation-full.xml ; done < $rsync_log
+  while read pdb_id ; do
+   rm -f $PDBML_VALID/$pdb_id-validation-full.xml
+  done < $rsync_log
  fi
 
  if [ -d $RDF_VALIT ] ; then
-  while read pdb_id ; do ; rm -f $RDF_VALID/$pdb_id-validation.rdf ; done < $rsync_log
+  while read pdb_id ; do
+   rm -f $RDF_VALID/$pdb_id-validation.rdf
+  done < $rsync_log
  fi
 
  rm -f $rsync_log
