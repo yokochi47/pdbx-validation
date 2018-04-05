b5a7aa2b5f2ebe72ab616a62fcdaba2066567795
yokochi47 <yokochi47@gmail.com>
2018-04-05 16:31:03 +0900

    Enable differential XML Schema validation of compressed PDBML-validation

--- scripts/validate_all_gz.sh
+++ scripts/validate_all_gz.sh
@@ -6,7 +6,7 @@ if [ ! -e $XSD2PGSCHEMA ] ; then
  ./scripts/update_extlibs.sh
 fi
 
-java -classpath $XSD2PGSCHEMA xmlvalidator --xsd $PDBX_VALIDATION_XSD --xml $XML_VALID/[0-9a-z]{2} --xml-file-ext gz
+java -classpath $XSD2PGSCHEMA xmlvalidator --xsd $PDBX_VALIDATION_XSD --xml $XML_VALID/[0-9a-z]{2} --xml-file-ext gz --verbose --sync chk_sum_pdbx_valid
 
 if [ $? != 0 ] ; then
  echo "$0 aborted."
