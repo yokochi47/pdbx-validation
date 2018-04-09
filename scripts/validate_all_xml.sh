459c16c7a54e3d6ef37ed76df4c1fa5f225c2f44
yokochi47 <yokochi47@gmail.com>
2018-04-09 13:55:26 +0900

    Append differential XML Schema validation for arbitrary XML directory

--- scripts/validate_all_xml.sh
+++ scripts/validate_all_xml.sh
@@ -23,7 +23,7 @@ if [ ! -e $XSD2PGSCHEMA ] ; then
  ./scripts/update_extlibs.sh
 fi
 
-java -classpath $XSD2PGSCHEMA xmlvalidator --xsd $PDBX_VALIDATION_XSD --xml $XML_DIR
+java -classpath $XSD2PGSCHEMA xmlvalidator --xsd $PDBX_VALIDATION_XSD --xml $XML_DIR --sync chk_sum_pdbx_valid
 
 if [ $? != 0 ] ; then
  echo "$0 aborted."
