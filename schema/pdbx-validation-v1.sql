44d01758094229a9a5dffb9730aeab23015ccbe4
Masashi Yokochi <yokochi47@gmail.com>
2018-08-03 11:21:31 +0900

    Merge

--- schema/pdbx-validation-v1.sql
+++ schema/pdbx-validation-v1.sql
@@ -34,8 +34,8 @@
 --
 
 --
--- PDBML-validation Schema v1.297
--- PDBXML-validation Schema translated from wwPDB Validation Information Dictionary v1.297, which is backward compatible with the PDBx/mmCIF Dictionary v5.297: http://mmcif.wwpdb.org/dictionaries/ascii/mmcif_pdbx_v50.dic
+-- PDBML-validation Schema v1.298
+-- PDBXML-validation Schema translated from wwPDB Validation Information Dictionary v1.298, which is backward compatible with the PDBx/mmCIF Dictionary v5.298: http://mmcif.wwpdb.org/dictionaries/ascii/mmcif_pdbx_v50.dic
 -- URI-reference = http://pdbml.pdb.org/schema/pdbx-validation-v1.xsd
 --
 
