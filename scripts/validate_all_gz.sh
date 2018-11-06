76a8e9c178ff303286995964af21766ee47c8abf
Masashi Yokochi <yokochi47@gmail.com>
2018-11-06 15:52:06 +0900

    Merge

--- scripts/validate_all_gz.sh
+++ scripts/validate_all_gz.sh
@@ -2,9 +2,29 @@
 
 source ./scripts/env.sh
 
+# Set PDBML-validation by default
+XML_DIR=$XML_VALID
+# Set PDBML-validation-alt
+#XML_DIR=$XML_VALID_ALT
+
+ARGV=`getopt --long -o "d:" "$@"`
+eval set -- "$ARGV"
+while true ; do
+ case "$1" in
+ -d)
+  XML_DIR=$2
+  shift
+ ;;
+ *)
+  break
+ ;;
+ esac
+ shift
+done
+
 if [ ! -e $XSD2PGSCHEMA ] ; then
  ./scripts/update_extlibs.sh
 fi
 
-java -classpath $XSD2PGSCHEMA xmlvalidator --xsd $PDBX_VALIDATION_XSD --xml $XML_VALID/[0-9a-z]{2} --xml-file-ext gz --sync chk_sum_pdbml_valid || ( echo $0 aborted. && exit 1 )
+java -classpath $XSD2PGSCHEMA xmlvalidator --xsd $PDBX_VALIDATION_XSD --xml $XML_DIR/[0-9a-z]{2} --xml-file-ext gz --sync chk_sum_pdbml_valid || ( echo $0 aborted. && exit 1 )
 
