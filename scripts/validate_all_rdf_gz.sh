f8f50c5c1e0904217ca376151f19a5575d12ba5e
Masashi Yokochi <yokochi47@gmail.com>
2019-01-08 14:55:23 +0900

    Merge

--- scripts/validate_all_rdf_gz.sh
+++ scripts/validate_all_rdf_gz.sh
@@ -45,7 +45,7 @@ if [ ! -z $RDF_DIR ] ; then
    rdf_file=$RDF_DIR/`basename $rdf_gz_file .gz`
    err_file=$RDF_DIR/validate_$rdf_gz_file.err
 
-   gunzip -c $rdf_gz_file > $rdf_file ; rapper -q -c $rdf_file 2> $err_file rm -f $rdf_file $err_file || ( cat $err_file && exit 1 )
+   gunzip -c $rdf_gz_file > $rdf_file ; rapper -q -c $rdf_file 2> $err_file && rm -f $rdf_file $err_file || ( rm -f $rdf_file $rdf_gz_file ; cat $err_file )
 
   done < $rdf_file_list
 
