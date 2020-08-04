b7657b1511b16fa650ce56e2f895bb45357a4643
Masashi Yokochi <yokochi47>
2020-08-05 00:05:25 +0900

    Update

--- scripts/transl_to_rdf_vrpt_worker.sh
+++ scripts/transl_to_rdf_vrpt_worker.sh
@@ -63,8 +63,8 @@ do
 
   if ( [ ! -e $rdf_vrpt_file ] && [ ! -e $rdf_vrpt_gz_file ] ) || [ -e $err_file ] ; then
 
-   #java -jar $SAXON -s:$pdbml_vrpt_file -xsl:$PDBMLV2RDF_XSL -o:$rdf_vrpt_file wurcs2glytoucan=$GLYTOUCAN_XML 2> $err_file && rm -f $err_file || ( cat $err_file && exit 1 )
-   xsltproc -o $rdf_vrpt_file --param wurcs2glytoucan $GLYTOUCAN_XML $PDBMLV2RDF_XSL $pdbml_vrpt_file 2> $err_file && rm -f $err_file || ( cat $err_file && exit 1 )
+   java -jar $SAXON -s:$pdbml_vrpt_file -xsl:$PDBMLV2RDF_XSL -o:$rdf_vrpt_file wurcs2glytoucan=$GLYTOUCAN_XML 2> $err_file && rm -f $err_file || ( cat $err_file && exit 1 )
+   #xsltproc -o $rdf_vrpt_file --param wurcs2glytoucan $GLYTOUCAN_XML $PDBMLV2RDF_XSL $pdbml_vrpt_file 2> $err_file && rm -f $err_file || ( cat $err_file && exit 1 )
 
    if [ $has_rapper_command != "false" ] ; then
     rapper -q -c $rdf_vrpt_file 2> $err_file && rm -f $err_file || ( cat $err_file && exit 1 )
