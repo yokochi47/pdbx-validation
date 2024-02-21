b6a3bc34e7d29910bc96b188f6c9ba9adf45645c
Masashi Yokochi <yokochi47@gmail.com>
2024-02-21 17:56:21 +0900

    Release v4.4.34

--- resource/pdbx-v50.sql
+++ resource/pdbx-v50.sql
@@ -44,8 +44,8 @@
 --
 
 --
--- PDBML Schema v5.383
--- PDBML Schema translated from the PDBx/mmCIF Dictionary v5.383: http://mmcif.wwpdb.org/dictionaries/ascii/mmcif_pdbx_v50.dic
+-- PDBML Schema v5.384
+-- PDBML Schema translated from the PDBx/mmCIF Dictionary v5.384: http://mmcif.wwpdb.org/dictionaries/ascii/mmcif_pdbx_v50.dic
 -- URI-reference = http://pdbml.pdb.org/schema/pdbx-v50.xsd
 --
 
