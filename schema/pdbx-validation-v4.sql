f8a98eb9066b878db01efdf8dc9b2a53da4d3d1e
Masashi Yokochi <yokochi47@gmail.com>
2024-09-27 16:33:10 +0900

    Release v4.4.44

--- schema/pdbx-validation-v4.sql
+++ schema/pdbx-validation-v4.sql
@@ -44,8 +44,8 @@
 --
 
 --
--- VRPT PDBML Schema v4.393
--- VRPT PDBML Schema translated from wwPDB Validation Information Dictionary v4.393, which is backward compatible with the PDBx/mmCIF Dictionary v5.370: http://mmcif.wwpdb.org/dictionaries/ascii/mmcif_pdbx_v50.dic
+-- VRPT PDBML Schema v4.394
+-- VRPT PDBML Schema translated from wwPDB Validation Information Dictionary v4.394, which is backward compatible with the PDBx/mmCIF Dictionary v5.370: http://mmcif.wwpdb.org/dictionaries/ascii/mmcif_pdbx_v50.dic
 -- URI-reference = http://pdbml.pdb.org/schema/pdbx-validation-v4.xsd
 --
 
